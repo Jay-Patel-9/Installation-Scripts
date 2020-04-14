424bffce6d1f2af6f90fcd7eee236c1a1ce08fb4
Jay Patel <itwithjay@gmail.com>
2020-04-14 13:46:33 +0530

    Added validation,  is MongoDB installed or not?

--- install-MongoDB-3.6.9-RedHat.sh
+++ install-MongoDB-3.6.9-RedHat.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-
-#going to yum directory
+#Checking mongodb is installed or not
+mongo --version > /dev/null
+if [ $? -ge 1 ]
+then
 cd /etc/yum.repos.d/
 
 #Creating repo file
 touch mongodb-org-3.6.repo
 
-#adding repo
+#checking & adding repo
+if [ -f /etc/yum.repos.d/mongodb-org-3.6.repo ]; then
+echo "[+] Repo file not found creating it!"
 cat >> mongodb-org-3.6.repo <<EOL
 [mongodb-org-3.6]
 name=MongoDB Repository
@@ -16,19 +20,31 @@ enabled=1
 gpgkey=https://www.mongodb.org/static/pgp/server-3.6.asc
 EOL
 
+else
+    echo "[-] Repo file found skipping..."
+fi
+
 #installing mongodb
 sudo yum install -y mongodb-org-3.6.9 mongodb-org-server-3.6.9 mongodb-org-shell-3.6.9 mongodb-org-mongos-3.6.9 mongodb-org-tools-3.6.9 
+echo ""
 
 #pin mongodb package
 exclude=mongodb-org,mongodb-org-server,mongodb-org-shell,mongodb-org-mongos,mongodb-org-tools
 
-echo "Starting MongoDB Service"
+echo "[+] Starting MongoDB Service.."
 service mongod start
-echo "MongoDB Service is started"
+echo "[+] Getting MongoDB Status.."
+echo ""
+service mongod status
+echo ""
+echo "-------------------------------------------------------------------------------------------------------------------------------"
+echo "[->] To start stop mongodb use 'service mongod start/stop/status'"
+echo ""
+echo "[->] MongoDB is installed, You may need to edit */etc/mongo.conf* to use another directory for storing data and log files."
+echo ""
+echo "[->] Default data-directory is /var/lib/mongo"
+echo "-------------------------------------------------------------------------------------------------------------------------------"
 
-echo "##############################################################################################################################"
-echo "To start stop mongodb use 'service mongod start/stop/status'"
-echo "MongoDB is installed on your machine, Now you need to configure your data and log directory on mongod.conf file inside /etc/"
-echo "Default data-directory is /var/lib/mongo"
-echo "Once configured restart MongoDB service."
-echo "##############################################################################################################################"
+else
+    echo "[->] MongoDB is already installed"
+fi
